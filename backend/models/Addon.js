6b417e8c06fed5e9b3678f3c4859daf710872ce8
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2026-04-27 23:24:45 +0700

    Update Addon.js

--- backend/models/Addon.js
+++ backend/models/Addon.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const AddonSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    description: { type: String, default: '' }, // Kolom baru untuk deskripsi
     price: { type: Number, default: 0 },
-    image: { type: String, required: true }, // URL Cloudinary
+    image: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Addon', AddonSchema);
