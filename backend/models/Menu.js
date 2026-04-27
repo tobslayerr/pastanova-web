e50741457d2f8c94fa7c3ad497a642bb874b60f0
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2026-04-27 23:07:17 +0700

    Update Menu.js

--- backend/models/Menu.js
+++ backend/models/Menu.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-const menuSchema = new mongoose.Schema({
+
+const MenuSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String },
+    description: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    images: [{ type: String }] // Array URL dari Cloudinary
+    images: [String],
+    // TAMBAHKAN INI:
+    addons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Addon' }]
 }, { timestamps: true });
-module.exports = mongoose.model('Menu', menuSchema);
+
+module.exports = mongoose.model('Menu', MenuSchema);
